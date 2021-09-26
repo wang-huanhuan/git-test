5ef9731d52cc9581263d05bc785b99550a44fa39
LAPTOP-1I0GCBHP\whh <1643590085@qq.com>
2021-09-26 11:09:47 +0800

    second commit

--- src/main/java/com/atguigu/Git/GitTest.java
+++ src/main/java/com/atguigu/Git/GitTest.java
@@ -3,6 +3,6 @@ package com.atguigu.Git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello GIt!");
-        System.out.println("Hello GIt2!");
+        System.out.println("Hello GIt22!");
     }
 }
