7858704ee450ee2b0771c4eef89d50739eb47d32
LAPTOP-1I0GCBHP\whh <1643590085@qq.com>
2021-09-26 11:31:40 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/Git/GitTest.java
+++ src/main/java/com/atguigu/Git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello GIt!");
         System.out.println("Hello GIt22!");
+        System.out.println("Hello GIt3!");
     }
 }
