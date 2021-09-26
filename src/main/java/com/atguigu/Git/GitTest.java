c3487dc7dcc9912098c40600747943544b89602d
LAPTOP-1I0GCBHP\whh <1643590085@qq.com>
2021-09-26 11:08:20 +0800

    first commit

--- src/main/java/com/atguigu/Git/GitTest.java
+++ src/main/java/com/atguigu/Git/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.Git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello GIt!");
+        System.out.println("Hello GIt2!");
     }
 }
